fa0ee3e4fa86d3f12c9c53025cd95832098cb4a7
Kranti Modi <kranti@gmail.com>
2018-09-11 10:09:41 -0700

    Updated Solution

--- LeetCode/Algorithms/Median_of_Two_Sorted_Arrays.py
+++ LeetCode/Algorithms/Median_of_Two_Sorted_Arrays.py
@@ -11,13 +11,14 @@ nums2 = [2,3]
 
 m = len(nums1)
 n = len(nums2)
+mid = (m + n) // 2
 m1 = 0
 n1 = 0
 
-temp = [0] * (m + n)
+temp = [0] * (mid + 1)
 t1 = 0
 
-while (t1 < m + n and m1 < m and n1 < n):
+while (t1 < mid + 1 and m1 < m and n1 < n):
     if (nums1[m1] < nums2[n1]):
         temp[t1] = nums1[m1]
         m1 += 1
@@ -26,17 +27,16 @@ while (t1 < m + n and m1 < m and n1 < n):
         n1 += 1
     t1 += 1
 
-while (m1 < m):
+while (m1 < m and t1 < mid + 1):
     temp[t1] = nums1[m1]
     t1 += 1
     m1 += 1
 
-while (n1 < n):
+while (n1 < n and t1 < mid + 1):
     temp[t1] = nums2[n1]
     t1 += 1
     n1 += 1
 
-mid = (m + n) // 2
 if (m + n) % 2 == 0:
     print((temp[mid] + temp[mid - 1]) / 2.0)
     #return (temp[mid] + temp[mid - 1]) / 2.0
